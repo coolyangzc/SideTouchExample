e6b6d01f632615c788a8c30941e499cec7e645e6
coolyangzc <yang-zc13@mails.tsinghua.edu.cn>
2017-12-18 15:10:51 +0800

    Add confidenceU&D

--- app/src/main/java/pcg/yzc/sidetouchexample/HandPostureDetector.java
+++ app/src/main/java/pcg/yzc/sidetouchexample/HandPostureDetector.java
@@ -8,22 +8,29 @@ public class HandPostureDetector {
 
     public HistoryValueContainer confidenceL = new HistoryValueContainer(2000, 0.01);
     public HistoryValueContainer confidenceR = new HistoryValueContainer(2000, 0.01);
+    public HistoryValueContainer confidenceU = new HistoryValueContainer(1000, 0.01);
+    public HistoryValueContainer confidenceD = new HistoryValueContainer(1000, 0.01);
 
     private HandPostureResult res = new HandPostureResult();
 
     public HandPostureResult predict(int[][] capa) {
         double confL = 0, confR = 0;
+        double gravity = 0, force = 0;
         int countL = 0, countR = 0, highestL = -1, highestR = -1;
         for (int j = 0; j < Common.CapaNum_H; j++) {
             if (capa[L][j] > 10) {
                 countL++;
                 if (highestL == -1)
                     highestL = j;
+                gravity += j * capa[L][j];
+                force += capa[L][j];
             }
             if (capa[R][j] > 10) {
                 countR++;
                 if (highestR == -1)
                     highestR = j;
+                gravity += j * capa[R][j];
+                force += capa[R][j];
             }
         }
         if (countL > 0 && countR == 0)
@@ -38,8 +45,26 @@ public class HandPostureDetector {
         long curTime = SystemClock.uptimeMillis();
         confidenceL.update(confL, curTime);
         confidenceR.update(confR, curTime);
+
+
+        if (force <= 10) {
+            confidenceU.update(0, curTime);
+            confidenceD.update(0, curTime);
+        } else {
+            gravity /= force;
+            if (gravity < Common.CapaNum_H / 2) {
+                confidenceU.update(1, curTime);
+                confidenceD.update(0, curTime);
+            } else {
+                confidenceU.update(0, curTime);
+                confidenceD.update(1, curTime);
+            }
+        }
+
         res.L = confidenceL.getValue();
         res.R = confidenceR.getValue();
+        res.U = confidenceU.getValue();
+        res.D = confidenceD.getValue();
         return res;
     }
 }
